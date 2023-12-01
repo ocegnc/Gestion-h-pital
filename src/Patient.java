618f3ac1529b8b53ed7771b88c76dad43918714e
ocegnc <oceane.guennec@gmail.com>
2023-12-01 11:19:14 +0100

    commit tarif

--- src/Patient.java
+++ src/Patient.java
@@ -7,5 +7,26 @@ public class Patient {
         this.numSecu=numSecu;
     }
 
-
+    public int payerTarif(int tempsOccupe, String typeLit, String typeChambre){
+        int tarif = 0;
+        if(typeLit.equals("ambulatoire")){
+            tarif+=tempsOccupe*15/(24*60*60);
+            if(typeChambre.equals("simple")){
+                tarif+=tarif*2;
+            }
+        }
+        if(typeLit.equals("moyen séjour")) {
+            tarif += tempsOccupe * 20 / (24 * 60 * 60 * 14);
+            if (typeChambre.equals("simple")) {
+                tarif += tarif * 2;
+            }
+        }
+        if(typeLit.equals("ambulatoire")) {
+            tarif += tempsOccupe * 25 / (24 * 60 * 60*30);
+            if (typeChambre.equals("simple")) {
+                tarif += tarif * 2;
+            }
+        }
+        return tarif;
+    }
 }
